2b9e8fa5c8c17a9e79825ad4595a7e4783de2ff8
Gonzalo Rodríguez Jurado <gonzalorodjur01@gmail.com>
2024-11-06 17:49:59 +0100

    error

--- libft/libft.h
+++ libft/libft.h
@@ -25,6 +25,7 @@ typedef struct s_list
 
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
+void	ft_error(void);
 void	*ft_calloc(size_t nitems, size_t size);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
