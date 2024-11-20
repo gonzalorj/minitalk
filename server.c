2a3a8e6cc719ae78653474ff986d4b0fa6c2d861
Gonzalo Rodriguez Jurado <gorodrig@c1r5s6.42madrid.com>
2024-11-20 09:39:22 +0100

    norm ok

--- server.c
+++ server.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gorodrig <gorodrig@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 09:32:34 by gorodrig          #+#    #+#             */
+/*   Updated: 2024/11/20 09:39:12 by gorodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <signal.h>
 #include "libft/libft.h"
 
-void receive_n_print(int received)
+void	receive_n_print(int received)
 {
-	static unsigned char c;
-	static int bit_i;
+	static unsigned char	c;
+	static int				bit_i;
 
 	bit_i++;
 	c |= (received == SIGUSR1);
@@ -18,12 +30,12 @@ void receive_n_print(int received)
 		c <<= 1;
 }
 
-int main (void)
+int	main(void)
 {
 	ft_printf("Client PID is: %i\n", getpid());
 	signal(SIGUSR2, receive_n_print);
 	signal(SIGUSR1, receive_n_print);
-	while(1)
+	while (1)
 		pause();
 	return (0);
 }
